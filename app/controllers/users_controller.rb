a26038e79ac6020cdf74a7300b021c9a66cf04d3
EC2 Default User <ec2-user@ip-172-31-12-239.ap-northeast-1.compute.internal>
2021-03-13 17:37:34 +0000

    commit3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   
   
 
-  # before_action :ensure_correct_user(params[:id]), {only: [:edit, :update, :destroy]}
 
   def show
   	@user = User.find(params[:id])

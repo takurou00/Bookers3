091029cd81e4c3942e03baa22474638518b6bc24
EC2 Default User <ec2-user@ip-172-31-12-239.ap-northeast-1.compute.internal>
2021-03-13 16:25:50 +0000

    finish_book

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-          has_many :books, dependent: :destroy
-          attachment :profile_image
+  has_many :books, dependent: :destroy
+  attachment :profile_image
+  validates :name, length: {in: 2..20}
+  validates :introduction, length: {maximum: 50}
 end
